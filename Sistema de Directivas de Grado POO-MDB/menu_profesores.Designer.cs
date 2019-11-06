b1c770fd5306be0aded916643a1dd3930ae0eece
Edward <edexorsoft@gmail.com>
2019-11-06 15:04:33 -0600

    Menu

---
+++ Sistema de Directivas de Grado POO-MDB/menu_profesores.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Sistema_de_Directivas_de_Grado_POO_MDB
+{
+    partial class menu_profesores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu_profesores));
+            this.nameTeacher = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDirectivas = new System.Windows.Forms.Button();
+            this.btnPadres = new System.Windows.Forms.Button();
+            this.btnAlumnos = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblDirectivas = new System.Windows.Forms.Label();
+            this.lblAlumnos = new System.Windows.Forms.Label();
+            this.lblPadres = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnAjustes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTeacher
+            // 
+            this.nameTeacher.AutoSize = true;
+            this.nameTeacher.BackColor = System.Drawing.Color.Transparent;
+            this.nameTeacher.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nameTeacher.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTeacher.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nameTeacher.Location = new System.Drawing.Point(153, 393);
+            this.nameTeacher.Name = "nameTeacher";
+            this.nameTeacher.Size = new System.Drawing.Size(39, 28);
+            this.nameTeacher.TabIndex = 72;
+            this.nameTeacher.Text = "lol";
+            this.nameTeacher.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(36, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 28);
+            this.label1.TabIndex = 71;
+            this.label1.Text = "Profesor:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnDirectivas
+            // 
+            this.btnDirectivas.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDirectivas.BackgroundImage")));
+            this.btnDirectivas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDirectivas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDirectivas.Location = new System.Drawing.Point(249, 186);
+            this.btnDirectivas.Name = "btnDirectivas";
+            this.btnDirectivas.Size = new System.Drawing.Size(111, 103);
+            this.btnDirectivas.TabIndex = 70;
+            this.btnDirectivas.UseVisualStyleBackColor = true;
+            // 
+            // btnPadres
+            // 
+            this.btnPadres.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPadres.BackgroundImage")));
+            this.btnPadres.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPadres.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPadres.Location = new System.Drawing.Point(455, 186);
+            this.btnPadres.Name = "btnPadres";
+            this.btnPadres.Size = new System.Drawing.Size(111, 103);
+            this.btnPadres.TabIndex = 69;
+            this.btnPadres.UseVisualStyleBackColor = true;
+            // 
+            // btnAlumnos
+            // 
+            this.btnAlumnos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAlumnos.BackgroundImage")));
+            this.btnAlumnos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAlumnos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlumnos.Location = new System.Drawing.Point(41, 186);
+            this.btnAlumnos.Name = "btnAlumnos";
+            this.btnAlumnos.Size = new System.Drawing.Size(111, 103);
+            this.btnAlumnos.TabIndex = 68;
+            this.btnAlumnos.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Maroon;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalir.Location = new System.Drawing.Point(655, 244);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(110, 45);
+            this.btnSalir.TabIndex = 67;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            // 
+            // lblDirectivas
+            // 
+            this.lblDirectivas.AutoSize = true;
+            this.lblDirectivas.BackColor = System.Drawing.Color.Transparent;
+            this.lblDirectivas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblDirectivas.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDirectivas.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblDirectivas.Location = new System.Drawing.Point(244, 292);
+            this.lblDirectivas.Name = "lblDirectivas";
+            this.lblDirectivas.Size = new System.Drawing.Size(115, 28);
+            this.lblDirectivas.TabIndex = 66;
+            this.lblDirectivas.Text = "Directiva";
+            this.lblDirectivas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAlumnos
+            // 
+            this.lblAlumnos.AutoSize = true;
+            this.lblAlumnos.BackColor = System.Drawing.Color.Transparent;
+            this.lblAlumnos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblAlumnos.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlumnos.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblAlumnos.Location = new System.Drawing.Point(39, 292);
+            this.lblAlumnos.Name = "lblAlumnos";
+            this.lblAlumnos.Size = new System.Drawing.Size(113, 28);
+            this.lblAlumnos.TabIndex = 65;
+            this.lblAlumnos.Text = "Alumnos";
+            this.lblAlumnos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPadres
+            // 
+            this.lblPadres.AutoSize = true;
+            this.lblPadres.BackColor = System.Drawing.Color.Transparent;
+            this.lblPadres.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblPadres.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPadres.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPadres.Location = new System.Drawing.Point(403, 292);
+            this.lblPadres.Name = "lblPadres";
+            this.lblPadres.Size = new System.Drawing.Size(216, 28);
+            this.lblPadres.TabIndex = 64;
+            this.lblPadres.Text = "Padres de familia";
+            this.lblPadres.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTitulo.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTitulo.Location = new System.Drawing.Point(182, 29);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(402, 88);
+            this.lblTitulo.TabIndex = 63;
+            this.lblTitulo.Text = "Sistema de Directivas\r\nde Grado";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAjustes
+            // 
+            this.btnAjustes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAjustes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAjustes.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjustes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAjustes.Location = new System.Drawing.Point(655, 172);
+            this.btnAjustes.Name = "btnAjustes";
+            this.btnAjustes.Size = new System.Drawing.Size(110, 45);
+            this.btnAjustes.TabIndex = 62;
+            this.btnAjustes.Text = "Ajustes";
+            this.btnAjustes.UseVisualStyleBackColor = false;
+            // 
+            // menu_profesores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nameTeacher);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDirectivas);
+            this.Controls.Add(this.btnPadres);
+            this.Controls.Add(this.btnAlumnos);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lblDirectivas);
+            this.Controls.Add(this.lblAlumnos);
+            this.Controls.Add(this.lblPadres);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnAjustes);
+            this.Name = "menu_profesores";
+            this.Text = "menu_profesores";
+            this.Load += new System.EventHandler(this.Menu_profesores_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameTeacher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDirectivas;
+        private System.Windows.Forms.Button btnPadres;
+        private System.Windows.Forms.Button btnAlumnos;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblDirectivas;
+        private System.Windows.Forms.Label lblAlumnos;
+        private System.Windows.Forms.Label lblPadres;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnAjustes;
+    }
+}
---
+++ Sistema de Directivas de Grado POO-MDB/menu_profesores.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Sistema_de_Directivas_de_Grado_POO_MDB
+{
+    public partial class menu_profesores : Form
+    {
+        String kodigo = "";
+        public menu_profesores(String id)
+        {
+            InitializeComponent();
+            String codigo = id;
+
+            kodigo = codigo;
+
+            SqlConnection conexion = Conexion.conectar();
+            SqlCommand comando = new SqlCommand("SELECT * FROM Personas per INNER JOIN Profesores pro ON per.IdPersona = pro.IdPersona WHERE pro.idProfesor=@profe", conexion);
+            comando.Parameters.Clear();
+            comando.Parameters.AddWithValue("@profe", codigo);
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+                String nombre = (registro["PrimerNombre"].ToString()) + " " + (registro["SegundoNombre"].ToString()) + " " + (registro["PrimerApellido"].ToString());
+                nameTeacher.Text = nombre;
+
+            }
+
+            conexion.Close();
+
+        }
+
+        private void Menu_profesores_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAlumnos_Click(object sender, EventArgs e)
+        {
+            AlumnosForm_Prof form = new AlumnosForm_Prof(kodigo);
+            form.Show();
+            this.Hide();
+        }
+
+        private void LblTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
